cbc6b0a668db127973829869201b1af13f03da91
Salim <salimcetinkaya@outlook.com>
2021-03-21 02:04:29 +0300

    20 Mart

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 
 
 @Injectable({
@@ -24,5 +25,9 @@ export class ProductService {
     console.log(newPath)
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
 
+  }
+
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"/products/add",product)
   }
 }
